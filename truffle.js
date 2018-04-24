dc3e6dcccff0ad5534779f9bbea9de81a86df0a1
Elliot Shohet <elliot.shohet@gmail.com>
2018-04-24 09:23:53 -0700

    Add live to truffle config

--- truffle.js
+++ truffle.js
@@ -6,5 +6,10 @@ module.exports = {
             network_id: "*", // match any network
             gas: "99999999"
         },
+        live: {
+            host: "127.0.0.1",
+            post: 8545,
+            network_id: "1",
+        }
     }
 };
