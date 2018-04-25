a3b745c66b18a1a9e5e0394e4497612e43a52eeb
Elliot Shohet <elliot.shohet@gmail.com>
2018-04-25 00:21:00 -0700

    Ready for deploy

--- truffle.js
+++ truffle.js
@@ -7,10 +7,12 @@ module.exports = {
             gas: "99999999"
         },
         live: {
-            host: "localhost",
+            host: '127.0.0.1',
             post: 8545,
-            network_id: "1",
-            from: "0xcab2f51d80bfe9965cdfb2692b24c5c28c8415af"
+            network_id: 1,
+            gasPrice: 5000000000, //5gwei
+            gas: 450000,
+            from: '0xCaB2F51D80bfe9965cDFB2692B24C5c28c8415AF'
         }
     }
 };
